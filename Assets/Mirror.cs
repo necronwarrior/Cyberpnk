001b39e2e2534570f7cb65136e1f0977b1c8d42e
unknown <1302534@MC10810.uad.ac.uk>
2017-09-16 14:32:21 +0100

    Created cull off shader, altered off player collisions

--- Assets/Mirror.cs
+++ Assets/Mirror.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Mirror : MonoBehaviour {
 
 	// Use this for initialization
+	Camera camera;
+	public bool flipHorizontal;
 	void Start () {
-		Matrix4x4 mat = GetComponent<Camera>().projectionMatrix;
-		mat *= Matrix4x4.Scale(new Vector3(-1, 1, 1));
-		GetComponent<Camera>().projectionMatrix = mat;
+		camera = GetComponent<Camera>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnPreCull() {
+		camera.ResetWorldToCameraMatrix();
+		camera.ResetProjectionMatrix();
+		Vector3 scale = new Vector3(flipHorizontal ? -1 : 1, 1, 1);
+		camera.projectionMatrix = camera.projectionMatrix * Matrix4x4.Scale(scale);
+	}
+	void OnPreRender () {
+		GL.invertCulling = flipHorizontal;
+	}
+
+	void OnPostRender () {
+		GL.invertCulling = false;
 	}
 
 }
--- Assets/Scripts/SpiderCam.cs
+++ Assets/Scripts/SpiderCam.cs
@@ -9,33 +9,57 @@ public class SpiderCam : MonoBehaviour {
 //	Vector3 somePos;
 	public float distance;
 
+
+	Material[] matList1 , matList2;
+	public Material shadowMat;
 	public GameObject MC1, MC2;
+	Transform Z1LevelContainer, Z2LevelContainer;
 
 	// Use this for initialization
 	void Start () {
 		transform.LookAt (Spider);
+		Z1LevelContainer = MC1.transform.parent.GetChild (0);
+		Z2LevelContainer = MC2.transform.parent.GetChild (0);
 
+		matList1 = new Material[Z1LevelContainer.childCount];
+
+		for (int i = 0; i < Z1LevelContainer.childCount; ++i) {
+			matList1[i] = Z1LevelContainer.GetChild (i).GetComponent<MeshRenderer> ().material;
+		}
+
+		matList2 = new Material[Z2LevelContainer.childCount];
+
+		for (int i = 0; i < Z2LevelContainer.childCount; ++i) {
+			matList2[i] = Z2LevelContainer.GetChild (i).GetComponent<MeshRenderer> ().material;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		if (Spider.transform.GetComponent<KeyboardContoller>().flipped !=0) {
-			if (Spider.transform.GetComponent<KeyboardContoller> ().flipped == 1) {
+			if (Spider.transform.GetComponent<KeyboardContoller> ().flipped == 2) {
 				MC2.SetActive(true);
-				for (int i = 0; i < MC1.transform.parent.childCount; ++i) {
-					//MC1.transform.parent.GetChild (i).GetComponent<Material> ().color = Color.black;
+				for (int i = 0; i < Z1LevelContainer.childCount; ++i) {
+					Z1LevelContainer.GetChild (i).GetComponent<MeshRenderer> ().material = shadowMat;
+				}
+				for (int i = 0; i < Z2LevelContainer.childCount; ++i) {
+					Z2LevelContainer.GetChild (i).GetComponent<MeshRenderer> ().material = matList2[i];
 				}
 
 				MC1.SetActive(false);
 			}else{
-				if(Spider.transform.GetComponent<KeyboardContoller> ().flipped == 2)
+				if(Spider.transform.GetComponent<KeyboardContoller> ().flipped == 1)
 				{
 					
 					MC1.SetActive(true);
-					for (int i = 0; i < MC2.transform.parent.childCount; ++i) {
-						//MC2.transform.parent.GetChild (i).GetComponent<Material> ().color = Color.black;
+					for (int i = 0; i < Z2LevelContainer.childCount; ++i) {
+						Z2LevelContainer.GetChild (i).GetComponent<MeshRenderer> ().material = shadowMat;
 					}
+					for (int i = 0; i < Z1LevelContainer.childCount; ++i) {
+						Z1LevelContainer.GetChild (i).GetComponent<MeshRenderer> ().material = matList1[i];
+					}
+
 					MC2.SetActive(false);
 				}
 			}
